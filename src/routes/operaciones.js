740e34c86eb17c38f46c14d5c8cbf84b904ce995
carloslavie <carloslavie@gmail.com>
2021-03-19 20:16:17 -0300

    sin terminar login

--- src/routes/operaciones.js
+++ src/routes/operaciones.js
@@ -1,6 +1,113 @@
 const express = require('express');
 const conexion = require('../db/conexion');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const unless = require('express-unless');
+const bcrypt= require('bcrypt');
+const { response } = require('express');
+
+router.use(express.json());
+
+// //autenticacion
+// const auth = (req, res, next)=>{
+//     try {
+//         let token = req.headers["Authorization"];
+//         console.log("esto es el token header", token)
+//         if(!token){
+//             throw new Error("No te encuentras logueado")
+//         }
+//         token = token.replace("Bearer ", "");
+//         jwt.verify(token, "secret", (err, user)=>{
+//             if(err){
+//                 throw new Error("Token invalido")
+//             }
+//         });
+//         next();
+//     } 
+//     catch (e) {
+//         res.status(403).send({message : e.message})
+//     }
+// }
+
+//  auth.unless = unless;
+//  router.use(auth.unless({
+//      path:[
+//          {url:'/login', methods : ['POST']},
+//          {url:'/registro', methods : ['POST']}
+//      ]
+//  }));
+
+// //REGISTRO
+
+// router.post("/registro", async (req, res) =>{
+//     try{
+//         const { nombre, apellido, email, clave } = req.body;
+        
+//         if(!nombre || !apellido || !clave || !email){
+//             throw new Error("No complestaste los campos");
+//         }
+
+//         let query = "SELECT * FROM usuarios WHERE email = ?";
+//         let respuesta = await conexion.query(query, [email]);
+
+//         if(respuesta.length > 0){
+//             throw new Error("Ese email ya se encuentra registrado");
+//         }
+
+//         const claveEncriptada = await bcrypt.hash(clave, 10);
+
+
+//         query = "INSERT INTO usuarios (nombre, apellido, email, clave) VALUES (?,?,?,?)";
+//         respuesta = await conexion.query(query, [nombre, apellido, email, claveEncriptada]);
+
+//         res.status(200).send({message : "Se registro correctamente"});
+//     }
+//     catch(e){
+//         res.status(413).send({message : e.message})
+//     }
+// });
+
+// //LOGIN
+
+// router.post("/login" , async (req,res)=>{
+
+//     const {usuario, clave } = req.body;
+
+//     try {
+//          if(!usuario || !clave){
+//              throw new Error("No completaste los campos");
+//          }
+
+//         let query =  "SELECT * FROM usuarios WHERE email = ?";
+//         let respuesta = await conexion.query(query, [usuario]);
+
+//          if(respuesta.length == 0){
+//              throw new Error ("El usuario no se encuentra registrado");
+//          }
+
+//         console.log(respuesta[0].clave);
+//         const claveEncriptada = respuesta[0].clave;
+
+//         if(!bcrypt.compareSync(clave, claveEncriptada)){
+//             throw new Error ("clave incorrecta");
+//         }
+//         const tokenData = {
+//             nombre: respuesta[0].nombre,
+//             apellido: respuesta[0].apellido,
+//             id: respuesta[0].id
+//         }
+ 
+//         const token = jwt.sign(tokenData, "secret", {
+//             expiresIn: 60*60*24
+//         })
+//         console.log("esto est token",token);
+//         res.status(200).send({token})
+       
+//     } 
+//     catch (e) {
+//         res.status(413).send({message : e.message});
+//     }
+// });
 
 
 //Inresar operacion
@@ -13,8 +120,8 @@ router.post("/operaciones", async (req, res) => {
         //    }        
         const fecha = new Date();
         console.log(fecha);                          
-        query = "INSERT INTO operaciones (monto, concepto, fecha, tipo) VALUES (?, ?, ?,?)";
-        respuesta = await conexion.query(query, [monto, concepto.toUpperCase(), fecha, tipo]);
+        let query = "INSERT INTO operaciones (monto, concepto, fecha, tipo) VALUES (?, ?, ?,?)";
+        let respuesta = await conexion.query(query, [monto, concepto.toUpperCase(), fecha, tipo]);
                 
         res.status(200).send({"respuesta" : respuesta});
     
@@ -92,24 +199,7 @@ router.get("/operaciones/ingresos", async (req,res) => {
          //const { persona_id } = req.body;
     
          try {
-             //verificar si existe el libro
-            //  let query = "SELECT * FROM libro WHERE id = ?";
-            //  let respuesta = await conexion.query(query, [id]);
-            //  console.log(respuesta.length);
-    
-            //  if (respuesta.length == 0) {
-            //      throw new Error("No se encuentra ese libro");
-            //  }
-    
-            //  verificar si está prestado
-            //  query = "SELECT persona_id FROM libro WHERE id = ? AND persona_id IS NULL";
-            //  respuesta = await conexion.query(query, [id]);
-    
-            //  if (respuesta.length == 0) {
-            //      throw new Error("Ese libro está prestado, no se puede borrar");
-            //  }
-    
-            //  si no está prestado, borrar
+             
              let query = 'DELETE FROM operaciones WHERE id = ?';
              let respuesta = await conexion.query(query, [id]);
     
@@ -122,79 +212,5 @@ router.get("/operaciones/ingresos", async (req,res) => {
          }
      });
 
-//  router.put("/libro/prestar/:id" , async (req, res)=>{ //Para modificar el campo persona_id para prestar libro
-//     const {persona_id} = req.body;
-//     const {id} = req.params;
-//     try{
-//         let query = "UPDATE libro SET persona_id = ? WHERE id = ?";
-//         let respuesta = await conexion.query(query, [persona_id, id]);
- 
-//          query = "SELECT * FROM libro WHERE id = ?";
-//          respuesta = await conexion.query(query, [id]);
-//          res.status(200).send({"response" : respuesta});
-//         }
-//         catch(error){
-//             res.status(413).send({"Error" : error.message});
-//         }
-
-//  });
-
-
-
-   
-// router.get('/operaciones/:id', async (req,res) => {
-//     const {id} = req.params;
-//   try {
-//       const query = 'SELECT * FROM operaciones WHERE id = ?';
-//       const respuesta = await conexion.query(query, [id]);
-      
-//       if(respuesta.length == 0){
-//         throw new Error("Ese libro no se esta en inventario");
-//         }
-
-//       res.status(200).send({"respuesta" : respuesta});
-//   } catch (error) {
-//       res.status(413).send({"Error" : error.message});
-//   }
-// });
-
-
-
-//  router.put("/libro/devolver/:id" , async (req, res)=>{ 
-  
-//     const {id} = req.params;
-//     try{
-//         let query = "SELECT * FROM libro WHERE id = ?";
-//         let respuesta = await conexion.query(query, [id]);
-               
-//         if(respuesta.length == 0){
-//             throw new Error("Ese libro no existe");
-//         }
-
-//          query = "SELECT persona_id FROM libro WHERE id = ? AND persona_id IS NOT NULL";
-//          respuesta = await conexion.query(query, [id]);
-               
-//         if(respuesta.length == 0){
-//             throw new Error("Ese libro no esta prestado");
-//         }
-        
-//         query = "UPDATE libro SET persona_id = NULL WHERE id = ?";
-//         respuesta = await conexion.query(query, [id]);
-
-//         query = "SELECT * FROM libro WHERE id = ?";
-//         respuesta = await conexion.query(query, [id]);
-//         res.status(200).send({"El libro fue devuelto correctamente" : respuesta});
-       
-//        }
-//        catch(error){
-//            res.status(413).send({"Error" : error.message});
-//        }
-// });
-
-// 
-
-// function isEmpty(str){
-//     return (!str || 0 == str.length);
-// }
 
 module.exports = router;
